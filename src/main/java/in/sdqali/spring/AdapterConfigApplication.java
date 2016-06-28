fed80b938fcc1486d7ac4dd2bea5f5f9aebae1e3
Sadique Ali <sadiqalikm@gmail.com>
2016-06-27 21:45:11 -0400

    Initial commit

---
+++ src/main/java/in/sdqali/spring/AdapterConfigApplication.java
@@ -0,0 +1,12 @@
+package in.sdqali.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdapterConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdapterConfigApplication.class, args);
+	}
+}
